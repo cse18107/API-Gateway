e95778a11d1a64dd9c8ef7f070afbe1ea4b7079c
cse18107 <cse18107@cemk.ac.in>
2024-08-12 00:49:48 +0530

    added signin with jwt creation

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,6 +6,6 @@ const userRouter = require('./user-route');
 const router = express.Router();
 
 router.get("/info", InfoController.info);
-router.use('/signup', userRouter)
+router.use('/user', userRouter)
 
 module.exports = router;
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,7 +1,8 @@
 const {UserRepository} = require('../repositories');
 const AppError = require('../utils/errors/app-error');
 const {StatusCodes} = require('http-status-codes');
-
+const { checkPassword } = require('../utils/common/auth');
+const { Auth } = require('../utils/common');
 const userRepo = new UserRepository();
 
 async function create(data) {
@@ -24,6 +25,28 @@ async function create(data) {
     }
 }
 
+async function signin(data) {
+    try {
+        const user = await userRepo.getUserByEmail(data.email);
+        if(!user){
+            throw new AppError('No user found for the given email', StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch = checkPassword(data.password, user.password);
+        if(!passwordMatch) {
+            throw new AppError('Invalid Password', StatusCodes.BAD_REQUEST);
+        }
+        const jwt = Auth.createToken({id: user.id, email: user.email});
+        return jwt;
+    } catch (error) {
+        if(error instanceof AppError) throw error;
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
+
+
+
 module.exports = {
-    create
+    create,
+    signin
 }

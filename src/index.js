72d85370ced6211cd7da470f976071a234407159
cse18107 <cse18107@cemk.ac.in>
2024-08-12 13:54:25 +0530

    Added rate limiter and proxy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 const express = require('express');
-
+const rateLimit = require('express-rate-limit');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 15 minutes
+  max: 3,
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+app.use(limiter);
+
+app.use(
+  '/flightsService',
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {'^/flightsService' : '/'}
+  }),
+);
+
+app.use(
+  '/flightBookingService',
+  createProxyMiddleware({
+    target: ServerConfig.FLIGHT_BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {'^/flightBookingService' : '/'}
+  }),
+);
 
 app.use("/api", apiRoutes);
 
